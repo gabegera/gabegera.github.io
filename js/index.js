b51af86dbd7189e67841c9a4f30c1d4c1db4190d
gabegera <gabegera2003@gmail.com>
2025-12-02 13:33:09 -0800

    Subsystems and Fullscreen Images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 ﻿function openLink(inLink) {
     window.open(inLink, "_blank");
 }
+
+function toggleFullscreen(inID) {
+    const element = document.getElementById(inID);
+
+    if (element) {
+        if (element.classList.contains("fullscreen-media")) {
+            element.classList.remove("fullscreen-media");
+            element.classList.add("mechanics-media");
+        }
+        else {
+            element.classList.remove("mechanics-media");
+            element.classList.add("fullscreen-media");
+        }
+    }
+}
